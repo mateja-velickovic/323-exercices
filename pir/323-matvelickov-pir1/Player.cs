07b5834f956fd64ab7f5ecbf4a33a065d864b310
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2024-09-27 12:10:26 +0200

    feat(exdj): Ajout des exercices du jour

---
+++ pir/323-matvelickov-pir1/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _323_matvelickov_pir1
+{
+    class Player
+    {
+        private readonly string _name;
+        private readonly int _age;
+
+        public Player(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public string Name => _name;
+
+        public int Age => _age;
+    }
+}
--- pir/323-matvelickov-pir1/Program.cs
+++ pir/323-matvelickov-pir1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,36 @@ namespace _323_matvelickov_pir1
     {
         static void Main(string[] args)
         {
+            // 4 players
+            var players = ImmutableList.Create(
+                new Player("Joe", 32),
+                new Player("Jack", 30),
+                new Player("William", 37),
+                new Player("Averell", 25)
+            );
+
+            Player x = FindOlder(players);
+
+            Player FindOlder(IEnumerable<Player> pl)
+            {
+                Player elder = pl.First();
+
+                for(int i = 0; i < 4; ++i)
+                {
+                    Player p = pl.ElementAt(i);
+                    if(p.Age > elder.Age)
+                    {
+                        elder = new Player(p.Name, p.Age);
+                    }
+                }
+
+                return elder;
+            }
+            
+            Console.WriteLine($"Le plus agé est {x.Name} qui a {x.Age} ans");
+
+            Console.ReadKey();
         }
+
     }
 }
